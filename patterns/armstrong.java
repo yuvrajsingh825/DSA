07a2d787370d985e2943e9be2b8dc58b7faed3bb
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-07-13 18:10:59 +0530

    add

---
+++ patterns/armstrong.java
@@ -0,0 +1,18 @@
+ class armstrong {
+    public static void main(String[] args) {
+        int num = 153, originalNum, remainder, result = 0;
+        originalNum = num;
+
+        while (originalNum != 0)
+        {
+            remainder = originalNum % 10;
+            result += Math.pow(remainder, 3);
+            originalNum /= 10;
+        }
+
+        if (result == num)
+            System.out.println(num + " is an Armstrong number.");
+        else
+            System.out.println(num + " is not an Armstrong number.");
+    }
+}

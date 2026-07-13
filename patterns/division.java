b99c34c0ce72eb1c8a289561da4a2c41db72102c
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-07-13 18:11:20 +0530

    add

---
+++ patterns/division.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class division {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter the number :");
+       int n = sc.nextInt();
+       for(int i =1;i<=n;i++){
+        if(n%i==0){
+            System.out.print(i+" ");
+        }
+
+        }
+       }
+       
+     
+    }
+    
